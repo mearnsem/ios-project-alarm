ef89a1c7c1412985b4560d4c98ed57593334ffba
Emily Mearns <mearnsem@gmail.com>
2016-05-23 14:47:24 -0600

    View Hierarchy
    and Custom TableViewCell & Static Table View complete

---
+++ Alarm/SwitchTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  SwitchTableViewCell.swift
+//  Alarm
+//
+//  Created by Emily Mearns on 5/23/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    // MARK: - IBOutlets & Properties
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var alarmSwitch: UISwitch!
+    
+    // MARK: - IBActions
+    
+    @IBAction func switchValueChanged(sender: AnyObject) {
+    }
+
+
+}
